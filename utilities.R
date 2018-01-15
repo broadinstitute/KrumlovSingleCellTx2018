9033c6399f4510e08bf28ef91212fe8c1988610e
Brian Haas <brianjohnhaas@gmail.com>
2018-01-15 12:55:07 +0100

    add

--- utilities.R
+++ utilities.R
@@ -179,3 +179,56 @@ NB.var.genes <- function(
 set.ifnull = function(a,b){
   if (is.null(a)) return(b)
 }
+
+topGOterms = function( fg.genes = NULL,
+                       bg.genes = NULL,
+                       organism = "Mouse", 
+                       ontology.use = "BP",
+                       stats.use = "fisher",
+                       algorithm.use = "weight01",
+                       topnodes.print=20,
+                       num.char=100){
+  
+  if (is.null(fg.genes) | is.null(bg.genes)){
+    stop("Error : Both gene lists are empty")
+  }
+  
+  require(topGO)
+  if (organism == "Mouse"){
+    mapping.use = "org.Mm.eg.db"
+    library(org.Mm.eg.db)
+  } else if (organism == "Human"){
+    mapping.use = "org.Hs.eg.db"
+    library(org.Hs.eg.db)
+  } else {
+    stop("Error : Organisms other than mouse not supported currently")
+  }
+  
+  n = length(bg.genes)
+  geneList = integer(n)
+  names(geneList) = bg.genes
+  geneList[intersect(names(geneList), fg.genes)]=1
+  print(paste0("Total ", length(geneList), " genes. ", sum(geneList), " genes in the foreground"))
+  geneList = factor(geneList)
+  
+  if (ontology.use %in% c("BP", "CC", "MF")){
+    print(paste0("Using Ontology : ", ontology.use))
+  } else {
+    stop("Error: Ontology not available. Should be one of BP, CC or MF")
+  }
+  # Make GO object
+  GOdata <- new("topGOdata",
+                description = "GOanalysis",
+                ontology = ontology.use,
+                allGenes = geneList,
+                annot = annFUN.org,
+                mapping = mapping.use,
+                ID = "SYMBOL",
+                nodeSize = 10)
+  print(paste0("Using the ", stats.use, " statistic with the ", algorithm.use, " algorithm"))
+  res.result <- runTest(GOdata, statistic = stats.use, algorithm = algorithm.use)
+  to.return = list()
+  to.return$GOdata = GOdata
+  to.return$res.table <- GenTable(GOdata, pval = res.result, topNodes = topnodes.print, numChar = num.char)
+  return(to.return)
+}
